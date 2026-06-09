77dd1576f52ff1db9a4a8feaba3052cd60f7f357
betokasa1982-del <betokasa1982@gmail.com>
2026-06-09 10:36:26 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // GPX Navigator Pro — Service Worker
 // Versão do cache: incrementar ao atualizar os arquivos
-const CACHE = "gpx-nav-v19";
+const CACHE = "gpx-nav-v20";
 const ASSETS = [
   "./",
   "./index.html",
